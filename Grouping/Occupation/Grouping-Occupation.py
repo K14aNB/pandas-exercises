262e1c8a87ca396ce1a1b206e1817a2fa79e14a4
K14aNB <kirnb@protonmail.com>
2024-04-24 08:29:05 +0530

    Updated setup steps in Grouping-Occupation

--- Grouping/Occupation/Grouping-Occupation.py
+++ Grouping/Occupation/Grouping-Occupation.py
@@ -17,36 +17,37 @@
 #
 
 # %% [markdown]
+# **Check and install the dependencies**
+
+# %%
+# !curl -sSL "https://raw.githubusercontent.com/K14aNB/pandas-exercises/main/requirements.txt"
+
+# %%
+# Run this command from terminal before running this notebook as .py script
+# Installs dependencies from requirements.txt present in the repo
+# %%capture
+# !pip install -r "https://raw.githubusercontent.com/K14aNB/pandas-exercises/main/requirements.txt"
+
+# %% [markdown]
 # **Import the libraries**
 
 # %%
 import pandas as pd
-import sys
+import env_setup
 import os
-from urllib3 import PoolManager,request
 
 # %% [markdown]
 # **Environment Setup**
 
 # %%
-# Setup Environment
-# Github gist url for Python Script which will perform environment setup
-env_url='https://gist.githubusercontent.com/K14aNB/46becb626d36ad8fa8d445616241dfef/raw/'
-http=PoolManager()
-response=http.request('GET',env_url)
-if response.status==200:
-    exec(response.data.decode('utf-8'))
-    setup(repo_path='pandas-exercises',nb_name='Grouping-Occupation')
+# Setup Environment(Downloading data and setting outputs specified in config.yaml)
+results_path=env_setup.setup(repo_name='pandas-exercises',nb_name='Grouping-Occupation')
 
 # %% [markdown]
 # **Read the data**
 
 # %%
-if 'google.colab' not in sys.modules:
-    dir=os.path.join(os.getcwd(),'Data Science','Git Repos','pandas-exercises')
-else:
-    dir=os.getcwd()
-users=pd.read_csv(os.path.join(dir,'data','u.user'),sep='|')
+users=pd.read_csv(os.path.join(results_path,'u.user'),sep='|')
 
 # %%
 users.head()
