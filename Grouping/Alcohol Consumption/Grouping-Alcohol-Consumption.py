0e398c1d23c05a6e54334d8cf20b97fe173f8b62
K14aNB <kirnb@protonmail.com>
2024-04-24 08:28:22 +0530

    Updated setup steps in Grouping-Alcohol-Consumption

--- Grouping/Alcohol Consumption/Grouping-Alcohol-Consumption.py
+++ Grouping/Alcohol Consumption/Grouping-Alcohol-Consumption.py
@@ -16,36 +16,37 @@
 # # **Grouping - Alcohol Consumption**
 
 # %% [markdown]
+# **Check and install the dependencies**
+
+# %%
+# !curl -sSL "https://raw.githubusercontent.com/K14aNB/pandas-exercises/main/requirements.txt"
+
+# %%
+# Run this command in terminal before running this notebook as .py script
+# Installs dependencies from requirements.txt present in the repo
+# %%capture
+# !pip install -r "https://raw.githubusercontent.com/K14aNB/pandas-exercises/main/requirements.txt"
+
+# %% [markdown]
 # **Import libraries**
 
 # %%
 import pandas as pd
-import sys
+import env_setup
 import os
-from urllib3 import PoolManager,request
 
 # %% [markdown]
 # **Environment Setup**
 
 # %%
-# Setup Environment
-# Github gist url for Python Script which will perform environment setup
-env_url='https://gist.githubusercontent.com/K14aNB/46becb626d36ad8fa8d445616241dfef/raw/'
-http=PoolManager()
-response=http.request('GET',env_url)
-if response.status==200:
-    exec(response.data.decode('utf-8'))
-    setup(repo_path='pandas-exercises',nb_name='Grouping-Alcohol-Consumption')
+# Setup Environment(Downloading data and setting output formats specified in config.yaml)
+results_path=env_setup.setup(repo_name='pandas-exercises',nb_name='Grouping-Alcohol-Consumption')
 
 # %% [markdown]
 # **Read the data**
 
 # %%
-if 'google.colab' not in sys.modules:
-    dir=os.path.join(os.getcwd,'Data Science','Git Repos','pandas-exercises')
-else:
-    dir=os.getcwd()
-drinks=pd.read_csv(os.path.join(dir,'data','drinks.csv'))
+drinks=pd.read_csv(os.path.join(results_path,'drinks.csv'))
 
 # %%
 drinks.head()
