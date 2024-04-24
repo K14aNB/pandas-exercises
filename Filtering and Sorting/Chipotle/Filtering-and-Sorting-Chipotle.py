14cb24147cb7d3acb52e52ea004ae9643921f79b
K14aNB <kirnb@protonmail.com>
2024-04-24 08:23:46 +0530

    Updated setup steps in Filtering-and-Sorting-Chipotle

--- Filtering and Sorting/Chipotle/Filtering-and-Sorting-Chipotle.py
+++ Filtering and Sorting/Chipotle/Filtering-and-Sorting-Chipotle.py
@@ -16,36 +16,37 @@
 # # **Filtering and Sorting - Chipotle**
 
 # %% [markdown]
+# **Check and install the dependencies**
+
+# %%
+# !curl -sSL 'https://raw.githubusercontent.com/K14aNB/pandas-exercises/main/requirements.txt'
+
+# %%
+# Run this command in terminal before running this notebook as .py script
+# Installs dependencies from requirements.txt present in the repo
+# %%capture
+# !pip install -r 'https://raw.githubusercontent.com/K14aNB/pandas-exercises/main/requirements.txt'
+
+# %% [markdown]
 # **Import the libraries**
 
 # %%
 import pandas as pd
 import os
-import sys
-from urllib3 import PoolManager,request
+import env_setup
 
 # %% [markdown]
 # **Environment Setup**
 
 # %%
-# Setup Environment
-# Github gist url for Python Script which will perform environment setup
-env_url = 'https://gist.githubusercontent.com/K14aNB/46becb626d36ad8fa8d445616241dfef/raw/'
-http = PoolManager()
-response = http.request('GET',env_url)
-if response.status==200:
-    exec(response.data.decode('utf-8'))
-    setup(repo_path='pandas-exercises',nb_name='Filtering-and-Sorting-Chipotle')
+# Setup Environment(Downloading data and setting output formats specified in config.yaml)
+result_path=env_setup.setup(repo_name='pandas-exercises',nb_name='Filtering-and-Sorting-Chipotle')
 
 # %% [markdown]
 # **Read the data**
 
 # %%
-if 'google.colab' not in sys.modules:
-    dir=os.path.join(os.getcwd(),'Data Science','Git Repos','pandas-exercises')
-else:
-    dir=os.getcwd()
-chipo=pd.read_csv(os.path.join(dir,'data','chipotle.tsv'),sep='\t')
+chipo=pd.read_csv(os.path.join(result_path,'chipotle.tsv'),sep='\t')
 
 # %%
 chipo.head()
